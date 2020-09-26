0aaf5279947700024932ae5c08016fa5f64e472d
RockyHomWang <rocky.H.W@outlook.com>
2020-09-26 11:45:47 +0800

    "截止到ExtentReport部分-xml配置文件部分（extentreportdemo.xml文件尚未完成）"

---
+++ Chapter6/src/main/java/com/ditcloud/extentreport/demo/TestMethodDemo.java
@@ -0,0 +1,33 @@
+package com.ditcloud.extentreport.demo;
+
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TestMethodDemo {
+
+    @Test
+    public void test1(){
+
+        Assert.assertEquals(1,2);
+    }
+
+    @Test
+    public  void test2(){
+
+        Assert.assertEquals(1,1);
+    }
+
+    @Test
+    public void logDemo(){
+        Reporter.log("这是日志的内容");
+        throw new RuntimeException("这是我自己运行时候的异常");
+    }
+
+    @Test
+    public void test3() {
+
+        Assert.assertEquals("HELLO", "hello");
+    }
+
+}
