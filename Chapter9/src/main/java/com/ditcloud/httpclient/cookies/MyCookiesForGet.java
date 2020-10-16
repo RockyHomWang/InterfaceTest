3afc89f7e7fe21743daa87cc5d01c7c25ea6b17c
RockyHomWang <rocky.H.W@outlook.com>
2020-10-16 18:46:11 +0800

    [Chapter9]截止“9-3 配置优化方法”的示例代码（httpclient）

---
+++ Chapter9/src/main/java/com/ditcloud/httpclient/cookies/MyCookiesForGet.java
@@ -0,0 +1,41 @@
+package com.ditcloud.httpclient.cookies;
+
+import java.io.IOException;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class MyCookiesForGet {
+
+    private String url;
+    private ResourceBundle bundle;
+
+    @BeforeTest
+    public void beforeTest(){
+        bundle = ResourceBundle.getBundle("application", Locale.CHINA);
+        url = bundle.getString("test.url");
+    }
+
+    @Test
+    public void testGetCookies() throws IOException {
+        String result;
+        //从配置文件中拼接测试的url
+        String uri= bundle.getString("getCookies.uri");
+        String testUrl = this.url + uri;
+
+        //测试逻辑代码书写
+        HttpGet get = new HttpGet(testUrl);
+        HttpClient client = new DefaultHttpClient();
+        HttpResponse response = client.execute(get);
+
+        result = EntityUtils.toString(response.getEntity(),"utf-8");
+        System.out.println(result);
+    }
+}
